34f301d1521f6324efae70aaeed836fc60cd84b3
Damian Wujec <wujecdamian12@gmail.com>
2026-06-24 22:08:23 +0200

    feat: create Log in form with proper passport logic & bcrypt compare

--- controllers/logInController.js
+++ controllers/logInController.js
@@ -1,9 +1,17 @@
+import passport from "passport";
+
 const renderForm = (req, res) => {
   res.render("logIn");
 };
 
-const logInUser = (req, res) => {
-  res.render("logIn");
+const logInUser = (req, res, next) => {
+  const auth = passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/",
+    failureMessage: true,
+  });
+  auth(req, res, next);
+  //You have to call this as function cause authenticate returns function ((revise))
 };
 
 export default { renderForm, logInUser };
