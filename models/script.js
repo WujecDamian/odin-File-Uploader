21a9d7170bca9582631b35fa85b805f9d9ddee7b
Damian Wujec <wujecdamian12@gmail.com>
2026-06-24 07:27:54 +0200

    feat: create prisma schema & migrate

---
+++ models/script.js
@@ -0,0 +1,53 @@
+import { create } from "node:domain";
+import { prisma } from "./lib/prisma.js";
+
+async function main() {
+  // Create a new user with a post
+  const user = await prisma.user.create({
+    data: {
+      fullname: "John Wick",
+      email: "jooohnieWick@gmail.com",
+      username: "jwick",
+      password: "12345678",
+      folders: {
+        create: {
+          folder_name: "Hello World",
+          files: {
+            create: {
+              filename: "Dog1.jpg",
+              file_url:
+                "https://www.goodhousekeeping.com/life/pets/g4531/cutest-dog-breeds/",
+              file_size: "1.6MB",
+            },
+          },
+        },
+      },
+    },
+    include: {
+      folders: {
+        include: {
+          files: true,
+        },
+      },
+    },
+  });
+  console.log("Created user:", user);
+
+  // Fetch all users with their posts
+  const allUsers = await prisma.user.findMany({
+    include: {
+      posts: true,
+    },
+  });
+  console.log("All users:", JSON.stringify(allUsers, null, 2));
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
