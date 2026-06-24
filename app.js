7a5fdf3bc57d24421bd990332e781d68ea8fa387
Damian Wujec <wujecdamian12@gmail.com>
2026-06-24 19:48:57 +0200

    feat: add prisma session store

--- app.js
+++ app.js
@@ -13,6 +13,34 @@ app.set("view engine", "ejs");
 // Set the views directory
 app.set("views", path.join(__dirname, "views"));
 
+//Passport
+import expressSession from "express-session";
+import "dotenv/config";
+import { PrismaPg } from "@prisma/adapter-pg"; // For other db adapters, see Prisma docs
+import { PrismaClient } from "../../generated/prisma/client";
+import { PrismaSessionStore } from "@quixo3/prisma-session-store";
+
+// DATABASE_URL defined in env file included in prisma.config.js; see Prisma docs
+const connectionString = `${process.env.DATABASE_URL}`;
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter });
+
+app.use(
+  expressSession({
+    cookie: {
+      maxAge: 7 * 24 * 60 * 60 * 1000, // ms
+    },
+    secret: "a santa at nasa",
+    resave: true,
+    saveUninitialized: true,
+    store: new PrismaSessionStore(prisma, {
+      checkPeriod: 2 * 60 * 1000, //ms
+      dbRecordIdIsSessionId: true,
+      dbRecordIdFunction: undefined,
+    }),
+  }),
+);
+
 // Routers
 import indexRouter from "./routes/indexRouter.js";
 import logInRouter from "./routes/logInRouter.js";
